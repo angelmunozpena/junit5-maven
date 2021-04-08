915e44cb61916a445b3d82a5597616094dff4644
Angel Muñoz <info@angelmunoz.es>
2021-04-08 12:24:15 +0200

    2021/04/08 historia de usuario Calculadora:suma y división

--- src/test/java/com/curso/LyfecicleTest.java
+++ src/test/java/com/curso/LyfecicleTest.java
@@ -1,5 +1,7 @@
 package com.curso;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -19,9 +21,15 @@ public class LyfecicleTest {
  
     @Test
     void regular_testi_method() {
-        //...
+        assertEquals(false, true);
+        
     }
  
+    @Test
+    void regular_testi_method2() {
+        assertEquals(4, 2+2);
+        
+    }
     @Test
     @Disabled("este tests no se ejecuta")
     void skippedTest() {
